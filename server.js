a5186d82a4fc1f3cdc5c93c9df01864b7bf4c234
Priyanshu Singh <singhrajput2457@gmail.com>
2026-02-14 01:48:04 +0530

    Added root route redirect to listings

--- server.js
+++ server.js
@@ -89,7 +89,9 @@ app.use((req, res, next) => {
     next();
 });
 
-
+app.get("/", (req, res) => {
+    res.redirect("/listings");
+});
 
 const listingRouter = require("./routes/listing.js")
 app.use("/listings", listingRouter);
