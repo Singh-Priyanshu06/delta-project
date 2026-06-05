90517084d64e743336db5c6de816e6cce68bf656
Priyanshu Singh <singhrajput2457@gmail.com>
2026-06-05 14:00:44 +0530

    Updated filter

--- server.js
+++ server.js
@@ -1,6 +1,4 @@
-if (process.env.NODE_ENV !== "production") {
-    require('dotenv').config();
-}
+require("dotenv").config();
 
 const { cloudinary, storage } = require('./cloudConfig.js'); 
 const express = require("express")
